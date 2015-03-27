5d973a4cd56835de0d7467df9569390c31d03baf
mzilic <mzilic@mono.hr>
2015-03-27 15:33:26 +0100

    - fix documentation typo

--- dist/baasic-angular-security.js
+++ dist/baasic-angular-security.js
@@ -271,7 +271,7 @@
                  **/
                 create: uriTemplateService.parse('permissions/'),
                 /**
-                 * Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [github](https://github.com/Baasic/uritemplate-js) page.
+                 * Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [GitHub](https://github.com/Baasic/uritemplate-js) page.
                  * @method tags.parse
                  * @example baasicPermissionsRouteService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
                  **/
