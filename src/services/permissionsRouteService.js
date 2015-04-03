2c2c7caad5f21a0b3efdec1cc945b8eb1a028d4d
Dijana <dijana@mono.hr>
2015-04-03 18:58:16 +0200

    Route template updated

--- src/services/permissionsRouteService.js
+++ src/services/permissionsRouteService.js
@@ -56,7 +56,7 @@
 				/**
 				* Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [GitHub](https://github.com/Baasic/uritemplate-js) page.
 				* @method tags.parse
-				* @example baasicPermissionsRouteService.parse('route/{?embed,fields,options}').expand({embed: '<embedded-resource>'});
+				* @example baasicPermissionsRouteService.parse('<route>/{?embed,fields,options}').expand({embed: '<embedded-resource>'});
 				**/					
                 parse: uriTemplateService.parse
             };
